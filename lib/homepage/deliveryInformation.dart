bde6f13e133d0be937100230253aab9733a94490
Karen <jcdepedro2@up.edu.ph>
2024-05-14 12:41:49 +0800

    for order information

---
+++ lib/homepage/deliveryInformation.dart
@@ -0,0 +1,125 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Delivery{
+  final String orderId;
+  final String company_name;
+  final String customer_email;
+  final Timestamp dateFiled;
+  final String phone;
+  final String point_person;
+  final String note;
+  final LoadingDelivery loadingDelivery;
+  final List<UnloadingDelivery> unloadingList;
+
+  Delivery(
+      this.company_name,
+      this.customer_email,
+      this.dateFiled,
+      this.phone,
+      this.point_person,
+      this.note,
+      this.loadingDelivery,
+      this.orderId,
+      this.unloadingList);
+
+}
+
+class LoadingDelivery{
+  final Timestamp loadingTimeDate;
+  final String cargoType;
+  final String loadingLocation;
+  final String route;
+  final String warehouse;
+  final int totalCartons;
+  final int weight;
+
+  LoadingDelivery(
+      this.cargoType,
+      this.loadingLocation,
+      this.route,
+      this.warehouse,
+      this.totalCartons,
+      this.loadingTimeDate,
+      this.weight);
+}
+
+class UnloadingDelivery{
+  final Timestamp unloadingTimeDate;
+  final int reference_num;
+  final String unloadingLocation;
+  final String route;
+  final String recipient;
+  final int quantity;
+  final int weight;
+
+
+  UnloadingDelivery(
+      this.reference_num,
+      this.unloadingLocation,
+      this.route,
+      this.recipient,
+      this.quantity,
+      this.unloadingTimeDate,
+      this.weight);
+}
+
+void submitDelivery(Delivery delivery){
+  FirebaseFirestore.instance.collection('Order').doc('${delivery.orderId}').set({
+    'company_name':delivery.company_name,
+    'customer_email':delivery.customer_email,
+    'date_filed':delivery.dateFiled,
+    'loading_id':'LS-${getDeliveryId(delivery.orderId)}',
+    'note': delivery.note,
+    'phone':delivery.phone,
+    'point_person':delivery.point_person,
+    'confirmed_status':false,
+  });
+
+  FirebaseFirestore.instance.collection('Order/${delivery.orderId}/LoadingSchedule').doc('LS-${getDeliveryId(delivery.orderId)}').set({
+    'cargoType':delivery.loadingDelivery.cargoType,
+    'loadingLocation':delivery.loadingDelivery.loadingLocation,
+    'loadingTime_Date' :delivery.loadingDelivery.loadingTimeDate,
+    'route':delivery.loadingDelivery.route,
+    'totalCartons':delivery.loadingDelivery.totalCartons,
+    'warehouse':delivery.loadingDelivery.warehouse,
+  });
+  int index =1;
+  for(var unloading in delivery.unloadingList){
+    FirebaseFirestore.instance.collection('Order/${delivery.orderId}/LoadingSchedule/'
+        'LS-${getDeliveryId(delivery.orderId)}/UnloadingSchedule')
+        .doc('US-${getDeliveryId(delivery.orderId)}-$index+').set({
+      'quantity':unloading.quantity,
+      'recipient' :unloading.recipient,
+      'reference_num':unloading.reference_num,
+      'unloadingLocation':unloading.unloadingLocation,
+      'unloadingTimestamp':unloading.unloadingTimeDate,
+      'weight':unloading.weight,
+    });
+    index++;
+  }
+
+
+}
+
+Future<String> getOrderNumber() async {
+  CollectionReference orderCollection =
+  FirebaseFirestore.instance.collection('Order');
+
+  QuerySnapshot<Object?> querySnapshot = await orderCollection.get();
+
+  if (querySnapshot.docs.isNotEmpty) {
+    int nextNumber = querySnapshot.docs.length + 1;
+    String nextId = 'OR-${nextNumber.toString().padLeft(3, '0')}';
+    return nextId;
+  } else {
+
+    return 'OR001';
+  }
+
+}
+
+String getDeliveryId(String orderId){
+  return orderId.substring(3);
+}
+
+
